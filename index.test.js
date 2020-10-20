177bea36a83951bd0e4850942d2c27bc41640d39
coderaiser <coderaiser@cloudcmd.io>
2020-10-20 10:51:21 +0300

    feature: index: migrate to postcss v8

--- index.test.js
+++ index.test.js
@@ -1,55 +1,53 @@
-var postcss = require('postcss');
+'use strict'
 
-var plugin = require('./');
+const postcss = require('postcss')
+const plugin = require('./')
 
-var test = function (input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then(function (result) {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
-};
+const check = async (input, output, options) => {
+    const from = 'example.css'
+    const result = await postcss([plugin(options)]).process(input, { from })
 
-describe('postcss-disabled', function () {
+    expect(result.css).toEqual(output)
+    expect(result.warnings()).toHaveLength(0)
+}
 
-    it('default behavior, no options', function () {
-        return test(
+describe('postcss-disabled', () => {
+    it('default behavior, no options', () => {
+        return check(
             '.foo:disabled { background-color: #f9f9f9; }',
-            '.foo:disabled, .foo[disabled] { background-color: #f9f9f9; }',
-            {}
-        );
-    });
+            '.foo:disabled, .foo[disabled] { background-color: #f9f9f9; }'
+        )
+    })
 
-    it('addAttributes: true', function () {
-        return test(
+    it('addAttributes: true', () => {
+        return check(
             '.foo:disabled { background-color: #f9f9f9; }',
             '.foo:disabled, .foo[disabled] { background-color: #f9f9f9; }',
             { addAttribute: true }
-        );
-    });
+        )
+    })
 
-    it('addAttributes: false', function () {
-        return test(
+    it('addAttributes: false', () => {
+        return check(
             '.foo:disabled { background-color: #f9f9f9; }',
             '.foo:disabled { background-color: #f9f9f9; }',
             { addAttribute: false }
-        );
-    });
+        )
+    })
 
-    it('addClass: true', function () {
-        return test(
+    it('addClass: true', () => {
+        return check(
             '.foo:disabled { background-color: #f9f9f9; }',
             '.foo:disabled, .foo.disabled, .foo[disabled] { background-color: #f9f9f9; }',
             { addClass: true }
-        );
-    });
+        )
+    })
 
-    it('addClass: false', function () {
-        return test(
+    it('addClass: false', () => {
+        return check(
             '.foo:disabled { background-color: #f9f9f9; }',
             '.foo:disabled, .foo[disabled] { background-color: #f9f9f9; }',
             { addClass: false }
-        );
-    });
-
-});
+        )
+    })
+})
